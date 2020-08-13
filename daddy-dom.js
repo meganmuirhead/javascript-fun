402c4324647656c8a7ad4d19cac12a453de5240c
Megan Muirhead <megan@tulgy.com>
2020-08-13 10:06:23 -0600

    working on first function

--- daddy-dom.js
+++ daddy-dom.js
@@ -3,16 +3,44 @@
 
 //create array for random things the sub did.
 // the user/daddy rates it from 1-10
+//
+// let subArr = [];
+//
+//
+// for(let i = 0; i< subArr.length; i ++) {
+//     subArr.push(subDid)
+//     console.log(subArr, 'arr');
+// }
+//
+// function onFormSubmit() {
+//     let subDid = document.getElementById("userInput").value;
+//     alert(subDid);
+// }
 
-let subArr = [];
+let daddysInput = process.argv[2];
 
 
-for(let i = 0; i< subArr.length; i ++) {
-    subArr.push(subDid)
-    console.log(subArr, 'arr');
+function gettingSubChore (question) {
+    let whatSubDidArr = ['Took dog for walk.',
+        'Made a coffee.',
+        'Made the bed.',
+        'Spilled a drink.',
+        'Dropped the soap.',
+        'Made the room messy.',
+        'Worked on the rigs.',
+        'Watered the garden.',
+        'Picked up the kids.',
+        'Worked on code.'
+    ]
+    let randomItem = whatSubDidArr[Math.floor(Math.random() * whatSubDidArr.length)]
+    console.log(randomItem, 'What does daddy rate the lils activity?');
+    // for(let i = 0; i < whatSubDidArr.length; i++) {
+    //     console.log(whatSubDidArr[i]);
+    //     if(whatSubDidArr[i] < ) {
+    //
+    //     }
+    // }
+
 }
 
-function onFormSubmit() {
-    let subDid = document.getElementById("userInput").value;
-    alert(subDid);
-}
+gettingSubChore()
