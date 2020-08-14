61ee1474c77e9a4527caeb4e45c5e77186f13dd3
Megan Muirhead <megan@tulgy.com>
2020-08-14 12:35:01 -0600

    working on code

--- daddy-dom.js
+++ daddy-dom.js
@@ -1,3 +1,5 @@
+const readline = require('readline');
+const rl = readline.createInterface({input: process.stdin, output: process.stdout})
 //randomize a prompt for what the sub did, then write an argument of a number between 1-10 to rate how Dom feels about it
 //Have different phrases to indicate how he feels, i.e. 5 = "Daddy is not super enthused, but you can still prove yourself"
 
@@ -7,20 +9,18 @@
 // let subArr = [];
 //
 //
-// for(let i = 0; i< subArr.length; i ++) {
-//     subArr.push(subDid)
-//     console.log(subArr, 'arr');
-// }
-//
 // function onFormSubmit() {
-//     let subDid = document.getElementById("userInput").value;
-//     alert(subDid);
+//     // let subDid = document.getElementById("userInput").value;
+//     return ratedByDaddy = prompt('what does daddy rate this? 1 being the worst 10 being the bestest')
+//
 // }
-
+// let daddysRatedFromHtml = onFormSubmit();
 let daddysInput = process.argv[2];
 
+// let daddysInput = process.argv[2];
 
-function gettingSubChore (question) {
+
+function gettingSubChore() {
     let whatSubDidArr = ['Took dog for walk.',
         'Made a coffee.',
         'Made the bed.',
@@ -33,7 +33,7 @@ function gettingSubChore (question) {
         'Worked on code.'
     ]
     let randomItem = whatSubDidArr[Math.floor(Math.random() * whatSubDidArr.length)]
-    console.log(randomItem, 'What does daddy rate the lils activity?');
+    console.log("Your sub ", randomItem);
     // for(let i = 0; i < whatSubDidArr.length; i++) {
     //     console.log(whatSubDidArr[i]);
     //     if(whatSubDidArr[i] < ) {
@@ -43,4 +43,32 @@ function gettingSubChore (question) {
 
 }
 
-gettingSubChore()
+gettingSubChore();
+
+rl.question('What does daddy rate the lil\'s activity? 10 being the best, 1 being the worst \n',
+    (num) => {
+        console.log(num)
+        parseInt(num.trim())
+        if (num < 4) {
+            return "You are grounded, go put your nose in the corner young lady!"
+        } else if (num > 4 && num <= 7) {
+            return "You are pushing it, daddy thinks you could be better!"
+        } else {
+            return "You are a really good girl! Daddy is please"
+
+        }
+    })
+// function daddyRate(num) {
+//     parseInt(num)
+//     if( num <  4) {
+//        return  "You are grounded, go put your nose in the corner young lady!"
+//     } else if ( num > 4 && num <= 7) {
+//         return  "You are pushing it, daddy thinks you could be better!"
+//     } else {
+//         return "You are a really good girl! Daddy is please"
+//
+//     }
+// }
+// let rate = daddyRate(daddysInput);
+// let rate = daddyRate(daddysRatedFromHtml);
+// console.log(rate)
